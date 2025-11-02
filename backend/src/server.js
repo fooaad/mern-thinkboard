94157afcf65bd060e1ee6e3ce81dddfcec13b6e8
Md Fuadul Islam <fuad@Fuads-Mac.local>
2025-11-03 01:12:33 +0600

    prepared for the deployment
    Signed-off-by: Md Fuadul Islam <fuad@Fuads-Mac.local>

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 //const express = require("express") // CommonJS syntax
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -12,15 +14,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 
 // middleware to parse JSON request bodies
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors({
-    origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV != "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        })
+    );
+}
+
 // our simple custom middleware
 app.use((req, res, next) => {
     console.log(`Request Method: ${req.method} & Request URL: ${req.url}`);
@@ -29,6 +35,13 @@ app.use((req, res, next) => {
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index"));
+    });
+}
 // What is an Endpoint?
 // An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource.
 
